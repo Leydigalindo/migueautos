ea8681e6b433474c7a0b21f6906d5444b440b32f
benyalf <joselinalfaro71@gmail.com>
2022-06-10 11:51:07 -0500

    elimine lo del compresor por que afectaba el modo claro y no :D

--- core/settings.py
+++ core/settings.py
@@ -59,7 +59,6 @@ INSTALLED_APPS = [
     'crispy_forms', #python -m pip install django-crispy-forms
     "crispy_tailwind", #pip install crispy-tailwind
     'allauth', #python -m pip install django-allauth
-    'compressor', # pip install django-compressor ==3.1
     
 ]
 
@@ -112,11 +111,7 @@ TEMPLATES = [
     },
 ]
 
-COMPRESS_ROOT = BASE_DIR / 'static'
 
-COMPRESS_ENABLED = True
-
-STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
