e0d047879565b6090d906e966fcc82c6f62b58b9
Leydigalindo <lmgalindo331@misena.edu.co>
2022-06-10 15:40:12 -0500

    Commit 1

--- servicios/forms.py
+++ servicios/forms.py
@@ -5,18 +5,18 @@ from servicios.models import Insumo, Marca, Servicio
 class ServicioForm(forms.ModelForm):
     class Meta:
         model= Servicio
-        fields=['nombre','tipodeservicio']
+        fields='__all__' 
         #fields= '__all__'
 class InsumoForm(forms.ModelForm):
     class Meta:
         model = Insumo
-        fields=['nombre','cantidad','marca']
+        fields='__all__' 
     
         #fields = '__all__'
         
 class MarcaForm(forms.ModelForm):
     class Meta:
         model = Marca
-        fields=['nombre']
+        fields='__all__' 
         #fields = '__all__'
     
--- servicios/urls.py
+++ servicios/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path
-from servicios.views import editarMarca, eliminarMarca, insumo, marca, servicio
+from servicios.views import editarInsumo, editarMarca, editarServicio, eliminarInsumo, eliminarMarca, eliminarServicio, insumo, marca, servicio
 urlpatterns = [
     path('servicio/',servicio, name='servicio'),
     path ('insumo/', insumo, name='insumo'),
     path ('marca/', marca, name='marca'),
     
-    #EDITAR Y ELIMINAR MARCA
+     #EDITAR Y ELIMINAR MARCA
     path ('marca/editarMarca/<int:id>', editarMarca, name='editarMarca'),
     path ('marca/eliminarMarca/<int:id>', eliminarMarca, name='eliminarMarca'),
+    path ('insumo/eliminarInsumo/<int:id>', eliminarInsumo, name='eliminarInsumo'),
+    path ('insumo/editarInsumo/<int:id>', editarInsumo, name='editarInsumo'),
+    path ('servicio/eliminarServicio/<int:id>', eliminarServicio, name='eliminarServicio'),
+    path ('servicio/editarServicio/<int:id>', editarServicio, name='editarServicio'),
 ]
